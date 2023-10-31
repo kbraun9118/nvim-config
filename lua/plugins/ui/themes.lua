da6f9ec17274083191427b89b9f2198ac41e26c3
Kurt Braun <kbraun9118@gmail.com>
2023-10-31 09:09:28 -0500

    Reconfigured telescope, and changed theme

--- lua/plugins/ui/themes.lua
+++ lua/plugins/ui/themes.lua
@@ -1,8 +1,15 @@
 return {
-	"catppuccin/nvim",
-	name = "catppuccin",
-	priority = 1000,
-	config = function()
-		vim.cmd.colorscheme("catppuccin-macchiato")
-	end,
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		priority = 1000,
+	},
+	{
+		"rmehri01/onenord.nvim",
+		name = "onenord",
+		priority = 1000,
+		config = function()
+			require("onenord").setup()
+		end,
+	},
 }
