43ecf23d8e43bd38d7909de20963a3b179c218ce
Kurt Braun <kbraun@shelterinsurance.com>
2023-10-31 12:08:57 -0500

    Fix issues from checkhealth

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -1,23 +1,23 @@
 return {
-	"theprimeagen/harpoon",
-  event  = "VeryLazy",
-	dependencies = {
-		"nvim/lua/plenary.nvim",
-	},
-	config = function()
-		local harpoon_ui = require("harpoon.ui")
+  "theprimeagen/harpoon",
+  event        = "VeryLazy",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+  },
+  config       = function()
+    local harpoon_ui = require("harpoon.ui")
 
-		vim.keymap.set("n", "<leader>hh", function()
-			harpoon_ui.nav_next()
-		end, { desc = "Next mark" })
-		vim.keymap.set("n", "<leader>hp", function()
-			harpoon_ui.nav_prev()
-		end, { desc = "Previous mark" })
-		vim.keymap.set("n", "<leader>hn", function()
-			require("harpoon.mark").add_file()
-		end, { desc = "Add mark" })
-		vim.keymap.set("n", "<leader>hm", function()
-			harpoon_ui.toggle_quick_menu()
-		end, { desc = "View marks" })
-	end,
+    vim.keymap.set("n", "<leader>hh", function()
+      harpoon_ui.nav_next()
+    end, { desc = "Next mark" })
+    vim.keymap.set("n", "<leader>hp", function()
+      harpoon_ui.nav_prev()
+    end, { desc = "Previous mark" })
+    vim.keymap.set("n", "<leader>hn", function()
+      require("harpoon.mark").add_file()
+    end, { desc = "Add mark" })
+    vim.keymap.set("n", "<leader>hm", function()
+      harpoon_ui.toggle_quick_menu()
+    end, { desc = "View marks" })
+  end,
 }
--- lua/plugins/treesitter/treesitter.lua
+++ lua/plugins/treesitter/treesitter.lua
@@ -1,32 +1,33 @@
 return {
-	"nvim-treesitter/nvim-treesitter",
-	dependencies = {
-		"windwp/nvim-ts-autotag",
-		"JoosepAlviste/nvim-ts-context-commentstring",
-		"nvim-treesitter/nvim-treesitter-textobjects",
-	},
-	event = { "BufReadPre", "BufNewFile" },
-	config = function()
-		require("nvim-treesitter.configs").setup({
-			highlight = {
-				enable = true,
-			},
-			autotag = {
-				enable = true,
-			},
-			context_commentstring = {
-				enable = true,
-			},
-			ensure_installed = {
-				"lua",
-				"typescript",
-				"javascript",
-				"html",
-				"css",
-				"rust",
-				"ocaml",
-				"go",
-			},
-		})
-	end,
+  "nvim-treesitter/nvim-treesitter",
+  dependencies = {
+    "windwp/nvim-ts-autotag",
+    "JoosepAlviste/nvim-ts-context-commentstring",
+    "nvim-treesitter/nvim-treesitter-textobjects",
+  },
+  event = { "BufReadPre", "BufNewFile" },
+  config = function()
+    require("nvim-treesitter.configs").setup({
+      highlight = {
+        enable = true,
+      },
+      auto_install = true,
+      autotag = {
+        enable = true,
+      },
+      context_commentstring = {
+        enable = true,
+      },
+      ensure_installed = {
+        "lua",
+        "typescript",
+        "javascript",
+        "html",
+        "css",
+        "rust",
+        "ocaml",
+        "go",
+      },
+    })
+  end,
 }
