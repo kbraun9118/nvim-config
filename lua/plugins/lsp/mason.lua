7a250268f921e507b66a6641b4873dbd95cf3f6e
Kurt Braun <kbraun9118@gmail.com>
2023-10-31 11:31:06 -0500

    remove angular languageserver from being auto installed similar to ocmal

--- lua/plugins/lsp/mason.lua
+++ lua/plugins/lsp/mason.lua
@@ -1,48 +1,47 @@
 return {
-  "williamboman/mason.nvim",
-  dependencies = {
-    "folke/neodev.nvim",
-    "williamboman/mason-lspconfig.nvim",
-  },
-  config = function()
-    local mason = require("mason")
+	"williamboman/mason.nvim",
+	dependencies = {
+		"folke/neodev.nvim",
+		"williamboman/mason-lspconfig.nvim",
+	},
+	config = function()
+		local mason = require("mason")
 
-    local mason_lspconfig = require("mason-lspconfig")
+		local mason_lspconfig = require("mason-lspconfig")
 
-    mason.setup({
-      ui = {
-        icons = {
-          package_installed = "✓",
-          package_pending = "➜",
-          package_uninstalled = "✗",
-        },
-      },
-    })
+		mason.setup({
+			ui = {
+				icons = {
+					package_installed = "✓",
+					package_pending = "➜",
+					package_uninstalled = "✗",
+				},
+			},
+		})
 
-    mason_lspconfig.setup({
-      ensure_installed = {
-        "tsserver",
-        "html",
-        "cssls",
-        "tailwindcss",
-        "svelte",
-        "prismals",
-        "pyright",
-        "rust_analyzer",
-        "lua_ls",
-        "volar",
-        "sqlls",
-        "gradle_ls",
-        "gopls",
-        "dockerls",
-        "docker_compose_language_service",
-        "angularls",
-      },
-      automatic_installation = {
-        exclude = { "ocamllsp" }
-      },
-    })
+		mason_lspconfig.setup({
+			ensure_installed = {
+				"tsserver",
+				"html",
+				"cssls",
+				"tailwindcss",
+				"svelte",
+				"prismals",
+				"pyright",
+				"rust_analyzer",
+				"lua_ls",
+				"volar",
+				"sqlls",
+				"gradle_ls",
+				"gopls",
+				"dockerls",
+				"docker_compose_language_service",
+			},
+			automatic_installation = {
+				exclude = { "ocamllsp" },
+			},
+		})
 
-    vim.keymap.set("n", "<leader>pm", "<cmd>Mason<CR>", { desc = "Open Mason" })
-  end,
+		vim.keymap.set("n", "<leader>pm", "<cmd>Mason<CR>", { desc = "Open Mason" })
+	end,
 }
