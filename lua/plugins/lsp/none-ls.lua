fe6f3ef54c503b65d21bebf2260d4e977b009ff1
Kurt Braun <kbraun@shelterinsurance.com>
2023-11-06 11:21:08 -0600

    Fix config for none-ls

--- lua/plugins/lsp/none-ls.lua
+++ lua/plugins/lsp/none-ls.lua
@@ -1,36 +1,17 @@
 return {
-  "nvimtools/none-ls.nvim",
+  "jay-babu/mason-null-ls.nvim",
   dependencies = {
-    "jay-babu/mason-null-ls.nvim",
-    cmd = { "NullLsInstall", "NullLsUninstall" },
-    opts = { handlers = {} },
+    "nvimtools/none-ls.nvim",
   },
   event = { "BufReadPre", "BufNewFile" },
   config = function()
-    local on_attach = function(client, bufnr)
-      local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
-
-      if client.supports_method("textDocument/formatting") then
-        vim.keymap.set("n", "<leader>lf", function()
-          vim.lsp.buf.format({ async = true })
-        end, { desc = "Format buffer" })
-        vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-        vim.api.nvim_create_autocmd("BufWritePre", {
-          group = augroup,
-          buffer = bufnr,
-          callback = function()
-            vim.lsp.buf.format({ async = false })
-          end,
-        })
-      end
-    end
-
-    require("null-ls").setup({
-      on_attach = on_attach
-    })
-
     require("mason-null-ls").setup({
-      ensure_installed = { "eslint_d" },
+      ensure_installed = {
+        "stylua"
+      },
+      automatic_installation = false,
+      handlers = {},
     })
+    require("null-ls").setup({})
   end,
 }
