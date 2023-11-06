72b176a2d61c2623750a69b989a667ba0168558a
Kurt Braun <kbraun9118@gmail.com>
2023-11-06 11:45:55 -0600

    Remove tailwind colors, causing issues when tailwindcss would disconnect

--- lua/plugins/lsp/lspconfig.lua
+++ lua/plugins/lsp/lspconfig.lua
@@ -1,111 +1,100 @@
 return {
-  "neovim/nvim-lspconfig",
-  event = { "BufReadPre", "BufNewFile" },
-  dependencies = {
-    "hrsh7th/cmp-nvim-lsp",
-    "nvim-lua/plenary.nvim",
-    { "antosha417/nvim-lsp-file-operations", config = true },
-    "williamboman/mason-lspconfig.nvim",
-    "folke/neodev.nvim",
-    "themaxmarchuk/tailwindcss-colors.nvim"
-  },
-  config = function()
-    require("neodev").setup({})
-    local lspconfig = require("lspconfig")
+	"neovim/nvim-lspconfig",
+	event = { "BufReadPre", "BufNewFile" },
+	dependencies = {
+		"hrsh7th/cmp-nvim-lsp",
+		"nvim-lua/plenary.nvim",
+		{ "antosha417/nvim-lsp-file-operations", config = true },
+		"williamboman/mason-lspconfig.nvim",
+		"folke/neodev.nvim",
+	},
+	config = function()
+		require("neodev").setup({})
+		local lspconfig = require("lspconfig")
 
-    local cmp_nvim_lsp = require("cmp_nvim_lsp")
-    local mason_lspconfig = require("mason-lspconfig")
+		local cmp_nvim_lsp = require("cmp_nvim_lsp")
+		local mason_lspconfig = require("mason-lspconfig")
 
-    local on_attach = function(client, bufnr)
-      local setkey = function(keys, cmd, desc)
-        vim.keymap.set("n", keys, cmd, { noremap = true, silent = true, desc = desc, buffer = bufnr })
-      end
+		local on_attach = function(client, bufnr)
+			local setkey = function(keys, cmd, desc)
+				vim.keymap.set("n", keys, cmd, { noremap = true, silent = true, desc = desc, buffer = bufnr })
+			end
 
-      setkey("gr", "<cmd>Telescope lsp_references<CR>", "Show LSP references")
-      setkey("gD", vim.lsp.buf.declaration, "Goto Declaration")
-      setkey("gd", "<cmd>Telescope lsp_definitions<CR>", "Show LSP definitions")
-      setkey("gi", "<cmd>Telescope lsp_implementations<CR>", "Show LSP implementations")
-      setkey("gt", "<cmd>Telescope lsp_type_definitions<CR>", "Show LSP type definitions")
-      setkey("<leader>la", vim.lsp.buf.code_action, "See available code actions")
-      setkey("<leader>lr", vim.lsp.buf.rename, "Smart rename")
-      setkey("<leader>lD", "<cmd>Telescope diagnostics bufnr=0<CR>", "Show buffer diagnostics")
-      setkey("<leader>ld", vim.diagnostic.open_float, "Show line diagnostics")
-      setkey("[d", vim.diagnostic.goto_prev, "Goto previous diagnostic")
-      setkey("]d", vim.diagnostic.goto_prev, "Goto next diagnostic")
-      setkey("K", vim.lsp.buf.hover, "Show documentation")
-      setkey("<leader>ls", "<cmd>LspRestart<CR>", "Restart LSP")
-      setkey("<leader>lf", function()
-        vim.lsp.buf.format({ async = true })
-      end, "Format buffer")
+			setkey("gr", "<cmd>Telescope lsp_references<CR>", "Show LSP references")
+			setkey("gD", vim.lsp.buf.declaration, "Goto Declaration")
+			setkey("gd", "<cmd>Telescope lsp_definitions<CR>", "Show LSP definitions")
+			setkey("gi", "<cmd>Telescope lsp_implementations<CR>", "Show LSP implementations")
+			setkey("gt", "<cmd>Telescope lsp_type_definitions<CR>", "Show LSP type definitions")
+			setkey("<leader>la", vim.lsp.buf.code_action, "See available code actions")
+			setkey("<leader>lr", vim.lsp.buf.rename, "Smart rename")
+			setkey("<leader>lD", "<cmd>Telescope diagnostics bufnr=0<CR>", "Show buffer diagnostics")
+			setkey("<leader>ld", vim.diagnostic.open_float, "Show line diagnostics")
+			setkey("[d", vim.diagnostic.goto_prev, "Goto previous diagnostic")
+			setkey("]d", vim.diagnostic.goto_prev, "Goto next diagnostic")
+			setkey("K", vim.lsp.buf.hover, "Show documentation")
+			setkey("<leader>ls", "<cmd>LspRestart<CR>", "Restart LSP")
+			setkey("<leader>lf", function()
+				vim.lsp.buf.format({ async = true })
+			end, "Format buffer")
 
-      vim.diagnostic.config({ update_in_insert = true })
+			vim.diagnostic.config({ update_in_insert = true })
 
-      local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+			local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
-      if client.supports_method("textDocument/formatting") then
-        vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-        vim.api.nvim_create_autocmd("BufWritePre", {
-          group = augroup,
-          buffer = bufnr,
-          callback = function()
-            vim.lsp.buf.format({ async = false })
-          end,
-        })
-      end
-    end
+			if client.supports_method("textDocument/formatting") then
+				vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+				vim.api.nvim_create_autocmd("BufWritePre", {
+					group = augroup,
+					buffer = bufnr,
+					callback = function()
+						vim.lsp.buf.format({ async = false })
+					end,
+				})
+			end
+		end
 
-    local capabilities = cmp_nvim_lsp.default_capabilities()
-    local handlers = {
-      ["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = "rounded" }),
-      ["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signatureHelp, { border = "rounded" }),
-    }
+		local capabilities = cmp_nvim_lsp.default_capabilities()
+		local handlers = {
+			["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = "rounded" }),
+			["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signatureHelp, { border = "rounded" }),
+		}
 
-    local signs = { Error = " ", Warn = " ", Hint = "󰠠 ", Info = " " }
-    for type, icon in pairs(signs) do
-      local hl = "DiagnosticSign" .. type
-      vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
-    end
+		local signs = { Error = " ", Warn = " ", Hint = "󰠠 ", Info = " " }
+		for type, icon in pairs(signs) do
+			local hl = "DiagnosticSign" .. type
+			vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+		end
 
-    -- let opam installed lsp take over
-    lspconfig["ocamllsp"].setup({
-      capabilities = capabilities,
-      on_attach = on_attach,
-      handlers = handlers,
-    })
+		-- let opam installed lsp take over
+		lspconfig["ocamllsp"].setup({
+			capabilities = capabilities,
+			on_attach = on_attach,
+			handlers = handlers,
+		})
 
-    mason_lspconfig.setup_handlers({
-      function(server)
-        lspconfig[server].setup({
-          capabilities = capabilities,
-          on_attach = on_attach,
-          handlers = handlers,
-        })
-      end,
-      ['tailwindcss'] = function(server)
-        lspconfig[server].setup({
-          capabilities,
-          on_attach = function(client, bufnr)
-            on_attach(client, bufnr)
-            require("tailwindcss-colors").buf_attach(bufnr)
-          end,
-          handlers = handlers
-        })
-      end,
-      ["lua_ls"] = function(server)
-        lspconfig[server].setup({
-          capabilities = capabilities,
-          on_attach = on_attach,
-          handlers = handlers,
-          settings = {
-            Lua = {
-              completion = {
-                callSnippet = "Replace",
-              },
-              workspace = { checkThirdParty = false },
-            },
-          },
-        })
-      end,
-    })
-  end,
+		mason_lspconfig.setup_handlers({
+			function(server)
+				lspconfig[server].setup({
+					capabilities = capabilities,
+					on_attach = on_attach,
+					handlers = handlers,
+				})
+			end,
+			["lua_ls"] = function(server)
+				lspconfig[server].setup({
+					capabilities = capabilities,
+					on_attach = on_attach,
+					handlers = handlers,
+					settings = {
+						Lua = {
+							completion = {
+								callSnippet = "Replace",
+							},
+							workspace = { checkThirdParty = false },
+						},
+					},
+				})
+			end,
+		})
+	end,
 }
