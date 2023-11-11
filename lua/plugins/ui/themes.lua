d86589d1dd562384c6668395a70a571d54bc9bfb
Kurt Braun <kbraun@kbraun.dev>
2023-11-10 19:12:06 -0600

    Switch to onedark theme

--- lua/plugins/ui/themes.lua
+++ lua/plugins/ui/themes.lua
@@ -8,8 +8,22 @@ return {
 		"rmehri01/onenord.nvim",
 		name = "onenord",
 		priority = 1000,
+	},
+	{
+		"navarasu/onedark.nvim",
+		name = "onedark",
+		priority = 1000,
 		config = function()
-			require("onenord").setup()
+			local onedark = require("onedark")
+			onedark.setup({
+				style = "warmer",
+			})
+			onedark.load()
 		end,
+	},
+	{
+		"tanvirtin/monokai.nvim",
+		name = "monokai",
+		priority = 1000,
 	},
 }
