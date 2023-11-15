1c8d0199a941391de113b955ce692a64e0cb106e
Kurt Braun <kbraun9118@gmail.com>
2023-11-15 07:06:59 -0600

    Added swiftformat

--- lua/plugins/lsp/none-ls.lua
+++ lua/plugins/lsp/none-ls.lua
@@ -1,17 +1,25 @@
 return {
-  "jay-babu/mason-null-ls.nvim",
-  dependencies = {
-    "nvimtools/none-ls.nvim",
-  },
-  event = { "BufReadPre", "BufNewFile" },
-  config = function()
-    require("mason-null-ls").setup({
-      ensure_installed = {
-        "stylua"
-      },
-      automatic_installation = false,
-      handlers = {},
-    })
-    require("null-ls").setup({})
-  end,
+	"jay-babu/mason-null-ls.nvim",
+	dependencies = {
+		"nvimtools/none-ls.nvim",
+	},
+	event = { "BufReadPre", "BufNewFile" },
+	config = function()
+		require("mason-null-ls").setup({
+			ensure_installed = {
+				"stylua",
+			},
+			automatic_installation = false,
+			handlers = {},
+		})
+		local null_ls = require("null-ls")
+		null_ls.setup({
+			sources = {
+				-- Swiftlint doesn't seem to work
+				-- null_ls.builtins.diagnostics.swiftlint,
+				-- null_ls.builtins.formatting.swiftlint,
+				null_ls.builtins.formatting.swift_format,
+			},
+		})
+	end,
 }
