624116d908834961f0df9089c9e0009215814673
Kurt Braun <kbraun@shelterinsurance.com>
2023-11-30 10:01:27 -0600

    Fix ts comment

--- lua/plugins/comment.lua
+++ lua/plugins/comment.lua
@@ -2,22 +2,28 @@ return {
 	{
 		"numToStr/Comment.nvim",
 		event = { "BufReadPre", "BufNewFile" },
-		opts = {
-			toggler = {
-				line = "<leader>/",
-				block = "<leader>\\",
-			},
-			opleader = {
-				line = "<leader>/",
-				block = "<leader>\\",
-			},
-		},
+		config = function()
+			require("Comment").setup({
+				toggler = {
+					line = "<leader>/",
+					block = "<leader>\\",
+				},
+				opleader = {
+					line = "<leader>/",
+					block = "<leader>\\",
+				},
+				pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
+			})
+		end,
 	},
 	{
 		"JoosepAlviste/nvim-ts-context-commentstring",
+		dependencies = {
+			"nvim-treesitter/nvim-treesitter",
+		},
 		config = function()
 			require("ts_context_commentstring").setup({
-				enable_autocmd = true,
+				enable_autocmd = false,
 			})
 
 			vim.g.skip_ts_context_commentstring_module = true
