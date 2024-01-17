c26d4c52b58aaae2b52b315cdf654c82a3cf7b86
Kurt Braun <kbraun@shelterinsurance.com>
2024-01-17 08:51:09 -0600

    Added TSUpdate to treesitter build

--- lua/plugins/treesitter/treesitter.lua
+++ lua/plugins/treesitter/treesitter.lua
@@ -1,29 +1,30 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  dependencies = {
-    "windwp/nvim-ts-autotag",
-    "nvim-treesitter/nvim-treesitter-textobjects",
-  },
-  event = { "BufReadPre", "BufNewFile" },
-  config = function()
-    require("nvim-treesitter.configs").setup({
-      highlight = {
-        enable = true,
-      },
-      auto_install = true,
-      autotag = {
-        enable = true,
-      },
-      ensure_installed = {
-        "lua",
-        "typescript",
-        "javascript",
-        "html",
-        "css",
-        "rust",
-        "ocaml",
-        "go",
-      },
-    })
-  end,
+	"nvim-treesitter/nvim-treesitter",
+	dependencies = {
+		"windwp/nvim-ts-autotag",
+		"nvim-treesitter/nvim-treesitter-textobjects",
+	},
+	event = { "BufReadPre", "BufNewFile" },
+	build = ":TSUpdate",
+	config = function()
+		require("nvim-treesitter.configs").setup({
+			highlight = {
+				enable = true,
+			},
+			auto_install = true,
+			autotag = {
+				enable = true,
+			},
+			ensure_installed = {
+				"lua",
+				"typescript",
+				"javascript",
+				"html",
+				"css",
+				"rust",
+				"ocaml",
+				"go",
+			},
+		})
+	end,
 }
