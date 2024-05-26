a71e445d7f6ea5b03d329f6cb63170ad181c0e7b
Kurt Braun <kbraun@kbraun.dev>
2024-05-26 15:15:46 -0500

    remove swift config

--- lua/plugins/lsp/none-ls.lua
+++ lua/plugins/lsp/none-ls.lua
@@ -13,13 +13,6 @@ return {
 			handlers = {},
 		})
 		local null_ls = require("null-ls")
-		null_ls.setup({
-			sources = {
-				-- Swiftlint doesn't seem to work
-				-- null_ls.builtins.diagnostics.swiftlint,
-				-- null_ls.builtins.formatting.swiftlint,
-				null_ls.builtins.formatting.swift_format,
-			},
-		})
+		null_ls.setup({})
 	end,
 }
