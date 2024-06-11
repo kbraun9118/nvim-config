d21389a6af369e95e8d4648b5f8c4e68fdccc41b
Kurt Braun <kbraun@shelterinsurance.com>
2024-06-11 09:43:04 -0500

    Added lualine harpoon and mason integration

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -1,23 +1,37 @@
 return {
-  "theprimeagen/harpoon",
-  event        = "VeryLazy",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-  },
-  config       = function()
-    local harpoon_ui = require("harpoon.ui")
+	"theprimeagen/harpoon",
+	branch = "harpoon2",
+	event = "VeryLazy",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+	},
+	config = function()
+		local harpoon = require("harpoon")
+		harpoon:setup()
 
-    vim.keymap.set("n", "<leader>hh", function()
-      harpoon_ui.nav_next()
-    end, { desc = "Next mark" })
-    vim.keymap.set("n", "<leader>hp", function()
-      harpoon_ui.nav_prev()
-    end, { desc = "Previous mark" })
-    vim.keymap.set("n", "<leader>hn", function()
-      require("harpoon.mark").add_file()
-    end, { desc = "Add mark" })
-    vim.keymap.set("n", "<leader>hm", function()
-      harpoon_ui.toggle_quick_menu()
-    end, { desc = "View marks" })
-  end,
+		vim.keymap.set("n", "<leader>hh", function()
+			harpoon:list():next()
+		end, { desc = "Next mark" })
+		vim.keymap.set("n", "<leader>hp", function()
+			harpoon:list():prev()
+		end, { desc = "Previous mark" })
+		vim.keymap.set("n", "<leader>hn", function()
+			harpoon:list():add()
+		end, { desc = "Add mark" })
+		vim.keymap.set("n", "<leader>hm", function()
+			harpoon.ui:toggle_quick_menu(harpoon:list())
+		end, { desc = "View marks" })
+		vim.keymap.set("n", "<leader>h1", function()
+			harpoon:list():select(1)
+		end, { desc = "Goto mark 1" })
+		vim.keymap.set("n", "<leader>h2", function()
+			harpoon:list():select(2)
+		end, { desc = "Goto mark 2" })
+		vim.keymap.set("n", "<leader>h3", function()
+			harpoon:list():select(3)
+		end, { desc = "Goto mark 3" })
+		vim.keymap.set("n", "<leader>h4", function()
+			harpoon:list():select(4)
+		end, { desc = "Goto mark 4" })
+	end,
 }
--- lua/plugins/ui/lualine.lua
+++ lua/plugins/ui/lualine.lua
@@ -1,33 +1,72 @@
 return {
-	"nvim-lualine/lualine.nvim",
-	dependencies = {
-		{ "nvim-tree/nvim-web-devicons", opt = true },
+	{
+		"letieu/harpoon-lualine",
+		dependencies = {
+			{ "ThePrimeagen/harpoon", branch = "harpoon2" },
+		},
 	},
-	event = "VeryLazy",
-	config = function()
-		local lualine = require("lualine")
-		local lazy_status = require("lazy.status")
+	{
 
+		"nvim-lualine/lualine.nvim",
+		dependencies = {
+			{ "nvim-tree/nvim-web-devicons", opt = true },
+		},
+		event = "VeryLazy",
+		config = function()
+			local lualine = require("lualine")
+			local lazy_status = require("lazy.status")
 
-		lualine.setup({
-			options = {
-				globalstatus = true,
-			},
-			sections = {
-				lualine_c = {
-					{ "filename" },
+			local function lualine_mason_updates()
+				local registry = require("mason-registry")
+				local installed_packages = registry.get_installed_packages()
+				local upgrades_available = false
+				local pacakges_outdated = 0
+
+				local function check_new_version(success, _)
+					if success then
+						upgrades_available = true
+						pacakges_outdated = pacakges_outdated + 1
+					end
+				end
+
+				for _, pkg in pairs(installed_packages) do
+					local p = registry.get_package(pkg.name)
+					if p then
+						p:check_new_version(check_new_version)
+					end
+				end
+
+				if upgrades_available then
+					return pacakges_outdated
+				else
+					return ""
+				end
+			end
+
+			lualine.setup({
+				options = {
+					globalstatus = true,
 				},
-				lualine_x = {
-					{
-						lazy_status.updates,
-						cond = lazy_status.has_updates,
-						color = { fg = "#ff9e64" },
+				sections = {
+					lualine_c = {
+						{ "filename" },
 					},
-					{ "encoding" },
-					{ "fileformat" },
-					{ "filetype" },
+					lualine_x = {
+						{ "harpoon2" },
+						{
+							lazy_status.updates,
+							cond = lazy_status.has_updates,
+							color = { fg = "#ff9e64" },
+						},
+						{
+							lualine_mason_updates,
+							color = { fg = "#aa4a44" },
+						},
+						{ "fileformat" },
+						{ "filetype" },
+					},
 				},
-			},
-		})
-	end,
+			})
+		end,
+	},
 }
