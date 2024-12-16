cada9a370c963c5aa2317c728683a315db5e0c96
Kurt Braun <kbraun@shelterinsurance.com>
2024-12-16 10:38:25 -0600

    added todo commands

--- lua/plugins/trouble.lua
+++ lua/plugins/trouble.lua
@@ -1,20 +1,20 @@
 return {
-	"folke/trouble.nvim",
-	dependencies = {
-		"nvim-tree/nvim-web-devicons",
-	},
-	opts = {},
-	cmd = "Trouble",
-	keys = {
-		{
-			"<leader>tt",
-			"<cmd>Trouble diagnostics toggle<cr>",
-			desc = "Diagnostics",
-		},
-		{
-			"<leader>tw",
-			"<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
-			desc = "Buffer Diagnostics",
-		},
-	},
+  "folke/trouble.nvim",
+  dependencies = {
+    "nvim-tree/nvim-web-devicons",
+  },
+  opts = {},
+  cmd = "Trouble",
+  keys = {
+    {
+      "<leader>td",
+      "<cmd>Trouble diagnostics toggle<cr>",
+      desc = "Diagnostics",
+    },
+    {
+      "<leader>tw",
+      "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+      desc = "Buffer Diagnostics",
+    },
+  },
 }
