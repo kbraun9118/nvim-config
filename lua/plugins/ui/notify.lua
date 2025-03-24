98ed12144d9daebe1fe6eafc562a5c92a994f0df
Kurt Braun <kbraun@shelterinsurance.com>
2025-03-24 07:39:48 -0500

    Add default background color for notify

--- lua/plugins/ui/notify.lua
+++ lua/plugins/ui/notify.lua
@@ -2,6 +2,10 @@ return {
 	"rcarriga/nvim-notify",
 	priority = 999,
 	config = function()
-		vim.notify = require("notify")
+		local notify = require("notify")
+		notify.setup({
+			background_colour = "#000000",
+		})
+		vim.notify = notify
 	end,
 }
