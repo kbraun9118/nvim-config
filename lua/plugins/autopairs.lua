d6de6d0b9d1f9cc232ae5a939982af4e0ccc0d60
Kurt Braun <kbraun@kbraun.dev>
2025-12-08 11:27:08 -0600

    added an autopair for generics

--- lua/plugins/autopairs.lua
+++ lua/plugins/autopairs.lua
@@ -2,5 +2,27 @@
 return {
 	"windwp/nvim-autopairs",
 	event = "InsertEnter",
-	opts = {},
+	config = function()
+		local npairs = require("nvim-autopairs")
+		npairs.setup({})
+
+		local Rule = require("nvim-autopairs.rule")
+		local cond = require("nvim-autopairs.conds")
+
+		npairs.add_rule(Rule("<", ">", {
+			-- if you use nvim-ts-autotag, you may want to exclude these filetypes from this rule
+			-- so that it doesn't conflict with nvim-ts-autotag
+			"-html",
+			"-javascriptreact",
+			"-typescriptreact",
+		}):with_pair(
+			-- regex will make it so that it will auto-pair on
+			-- `a<` but not `a <`
+			-- The `:?:?` part makes it also
+			-- work on Rust generics like `some_func::<T>()`
+			cond.before_regex("%a+:?:?$", 3)
+		):with_move(function(opts)
+			return opts.char == ">"
+		end))
+	end,
 }
